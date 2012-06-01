e6b59fb1b10266c7ea1e8a5a64561381923f2709
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-06-01 17:20:40 +0900

    add flip

---
+++ application/controllers/user/category.php
@@ -0,0 +1,31 @@
+<?php
+class Category extends MY_Controller {
+
+	function __construct() {
+		parent::__construct();
+		$this->load->model("Category_model");
+	}
+	
+	function index() {
+	}
+	
+	function test() {
+		$id = $this->input->get("id", true);
+		$this->list = $this->Category_model->get_list($id);
+		$data = array();
+		foreach($this->list->result_array() as $row) {
+			$data[] = array(
+				"attr" => array(
+					"id" => "node_".$row["id"],
+					"rel" => "default"
+				),
+				"data" => $row["name"],
+				"state" => ($row["child_cnt"] > 0) ? "closed" : ""
+			);
+		}
+		print json_encode($data);
+	}
+	
+	function node() {
+	}
+}
---
+++ application/controllers/user/tag.php
@@ -0,0 +1,29 @@
+<?php
+class Tag extends MY_Controller {
+	
+	function index() {
+		
+	}
+	
+	function search() {
+		print '[
+	"Basic",
+	"Closure",
+	"Cobol",
+	"Delphi",
+	"Erlang",
+	"Fortran",
+	"Go",
+	"Groovy",
+	"Haskel",
+	"Java",
+	"JavaScript",
+	"OCAML",
+	"PHP",
+	"Perl",
+	"Python",
+	"Ruby",
+	"Scala"
+]';
+	}
+}
