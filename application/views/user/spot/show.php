ff59b10cefaeb637897533694ead8112ac4078b1
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-06-18 19:46:20 +0900

    show spot template

---
+++ application/views/user/spot/show.php
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+$(function() {
+	$('#facebook_like').socialbutton('facebook_like', {
+		button: 'box_count',
+		url: 'http://p0009.kiyomizu.mac/user/spot/show/1'
+	});
+});
+</script>
+<pre>
+<?php
+	print_r($data);
+?>
+</pre>
+<div id="facebook_like"></div>
