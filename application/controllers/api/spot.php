a4488059ec630e66a6e54adf84c741b6028ad923
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-07-24 23:55:13 +0900

    DB操作系はなるべくモデル内で実装

--- application/controllers/api/spot.php
+++ application/controllers/api/spot.php
@@ -4,23 +4,35 @@ class Spot extends MY_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model("Spot_model");
+		$this->load->model("Tag_model");
+		$this->load->model("Category_model");
 	}
 		
 	function index() {
-		$fields = array();
-		$wheres = array();
-		$limit = 20;
-		$page = 1;
-		$offset = ($page - 1 ) * $limit;
-		$sorts = array("created_time" => "desc");
+		$request = array(
+				"ne_x"		=> $this->input->get("ne_x"),
+				"sw_x"		=> $this->input->get("sw_x"),
+				"ne_y"		=> $this->input->get("ne_y"),
+				"sw_y"		=> $this->input->get("sw_y"),
+				"category"	=> $this->input->get("category"),
+				"keyword"	=> $this->input->get("keyword"),
+		);
+		$page 		= $this->input->get("page");
+		$limit		= $this->input->get("limit");
+		$sort		= $this->input->get("sort");
+		$sort_type	= $this->input->get("sort_type");
 		
-		$data = $this->Spot_model->select($fields, $wheres, $limit, $offset, $sorts);
-		$result["count"] = $data->num_rows();
-		$result["list"] = array();
-		if ($data->num_rows() > 0) {
-			foreach($data->result_array() as $row)
-			$result["list"][] = $row;
+		if (!$limit) $limit = 20;
+		if (!$page) $page = 1;
+		$offset = ($page - 1) * $limit;
+		$request["tags"] = $this->Tag_model->tag_keys(array($condition["keyword"]));
+		$spot = $this->Spot_model->search($request, $offset, $limit, null, $sort, $sort_type);
+		if ($spot["list"]) {
+			$spot["relation"]["categories"] = $this->Category_model->get_names($spot["relation"]["categories"]);
+			$spot["relation"]["tags"] 		= $this->Tag_model->tag_values($spot["relation"]["tags"]);
 		}
-		print json_encode($result);
+// 		print "<pre>";
+// 		print_r($spot);
+		print json_encode($spot);
 	}
 }
--- application/controllers/api/tour.php
+++ application/controllers/api/tour.php
@@ -4,23 +4,35 @@ class Tour extends MY_Controller {
 	function __construct() {
 		parent::__construct();
 		$this->load->model("Tour_model");
+		$this->load->model("Tag_model");
+		$this->load->model("Category_model");
 	}
 	
 	function index() {
-		$fields = array();
-		$wheres = array();
-		$limit = 20;
-		$page = 1;
-		$offset = ($page - 1 ) * $limit;
-		$sorts = array("created_time" => "desc");
-		
-		$data = $this->Tour_model->select($fields, $wheres, $limit, $offset, $sorts);
-		$result["count"] = $data->num_rows();
-		$result["list"] = array();
-		if ($data->num_rows() > 0) {
-			foreach($data->result_array() as $row)
-			$result["list"][] = $row;
+		$request = array(
+			"ne_x"		=> $this->input->get("ne_x"),
+			"sw_x"		=> $this->input->get("sw_x"),
+			"ne_y"		=> $this->input->get("ne_y"),
+			"sw_y"		=> $this->input->get("sw_y"),
+			"category"	=> $this->input->get("category"),
+			"keyword"	=> $this->input->get("keyword"),
+		);
+		$page 		= $this->input->get("page");
+		$limit		= $this->input->get("limit");
+		$sort		= $this->input->get("sort");
+		$sort_type	= $this->input->get("sort_type");
+
+		$offset = ($page - 1) * $limit;
+		$request["tags"] = $this->Tag_model->tag_keys(array($condition["keyword"]));
+		$tour = $this->Tour_model->search($request, $offset, $limit, null, $sort, $sort_type);
+		if ($tour["list"]) {
+			$routes = $this->Tour_model->get_routes(array_keys($tour["list"]));
+			foreach ($routes as $tour_id => $route) {
+				$tour["list"][$tour_id]["routes"] = $route;
+			}
+			$tour["relation"]["categories"] = $this->Category_model->get_names($tour["relation"]["categories"]);
+			$tour["relation"]["tags"] 		= $this->Tag_model->tag_values($tour["relation"]["tags"]);
 		}
-		print json_encode($result);
+		print json_encode($tour);
 	}
 }
