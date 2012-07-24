ee44db26c63e281a215fe3e6be8d5105652c7048
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-07-24 23:52:18 +0900

    ユーザページテンプレート

--- application/views/user/top.php
+++ application/views/user/top.php
@@ -1,50 +1,10 @@
-<script type="text/javascript">
-$(function() {
-	$.ajax({
-		url: "<?php echo base_url("api/tour"); ?>",
-		data: {
-			type: "mydata"
-		},
-		dataType: "json",
-		success: function(json) {
-			if (json["count"] > 0) {
-				$(json["list"]).each(function() {
-					$("#tour_list").append('<li>'+this.name+' [<a href="<?php echo base_url("user/tour/form/");?>/'+this.id+'">編集</a>]</li>');
-				});
-			}
-		}
-	});
-	
-	$.ajax({
-		url: "<?php echo base_url("api/spot"); ?>",
-		data: {
-			type: "mydata"
-		},
-		dataType: "json",
-		success: function(json) {
-			if (json["count"] > 0) {
-				$(json["list"]).each(function() {
-					$("#spot_list").append('<li>'+this.name+' [<a href="<?php echo base_url("user/spot/form/");?>/'+this.id+'">編集</a>]</li>');
-				});
-			}
-		}
-	});
-	
-	// <li>bbb [<a href="<?php echo base_url("user/spot/update");?>">編集</a>]</li>
-	
-});
-</script>
-
-<a href="<?php echo base_url(""); ?>">TOP</a> &gt;
-ユーザーページ
+<script type="text/javascript" src="<?php echo base_url("assets"); ?>/js/apps/user/top.js"></script>
+<?php $this->load->view("contents_header"); ?>
 <h3>トップページ</h3>
-
 <div>あなたが登録したスケジュール一覧</div>
 [<a href="<?php echo base_url("user/tour/form");?>">追加</a>]<br />
-<ul id="tour_list">
-</ul>
+<ul id="tour_list"></ul>
 
 <div>あなたが登録した位置情報一覧</div>
 [<a href="<?php echo base_url("user/spot/form");?>">追加</a>]
-<ul id="spot_list">
-</ul>
+<ul id="spot_list"></ul>
