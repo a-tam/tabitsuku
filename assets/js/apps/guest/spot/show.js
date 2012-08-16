68df3d25428f832fa01e9365de7cb239cc6338c2
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-08-16 22:58:19 +0900

    スポットの位置表示、マーカー表示

--- assets/js/apps/guest/spot/show.js
+++ assets/js/apps/guest/spot/show.js
@@ -1,9 +1,18 @@
 $(function() {
-	latlng = new google.maps.LatLng(35.6894875, 139.69170639999993);
+	var lat = $("#pg_spot_info").attr("data-lat");
+	var lng = $("#pg_spot_info").attr("data-lng");
+	var name = $("#pg_name").text();
+	latlng = new google.maps.LatLng(lat, lng);
 	var myOptions = {
 		zoom: 10,
 		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map($("#pg_map")[0], myOptions);
+	var marker = new google.maps.Marker({
+		map			: map,
+		position	: latlng,
+		title		: name,
+		draggable	: false
+	});
 });
