c37a7d4cc06ae7bb95ebe7246f76d9c9c9588647
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-08-16 22:57:48 +0900

    ツアーのルート表示

--- assets/js/apps/guest/tour/show.js
+++ assets/js/apps/guest/tour/show.js
@@ -6,4 +6,55 @@ $(function() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map($("#pg_map")[0], myOptions);
+	var path = [];
+	
+	var lat_min = lat_max = lng_min = lng_max = null;
+	$(".pg_spot").each(function(i, elm) {
+		var id = $(elm).attr("data-spot-id");
+		if (id) {
+			var lat = $(elm).attr("data-lat");
+			var lng = $(elm).attr("data-lng");
+			if (lat_min == null) {
+				lat_min = lat;
+				lat_max = lat;
+				lng_min = lng;
+				lng_max = lng;
+			}
+			if (lat <= lat_min) { lat_min = lat; }
+			if (lat >  lat_max) { lat_max = lat; }
+			if (lng <= lng_min) { lng_min = lng; }
+			if (lng >  lng_max) { lng_max = lng; }
+			latlng = new google.maps.LatLng(lat, lng);
+			marker = new google.maps.Marker({
+				map			: map,
+				position	: latlng
+			});
+			
+			if (i == 0) {
+				marker.setIcon(gAssetUrl + '/img/map/icons/start.png');
+			} else if(i == $(".pg_spot").length - 1) {
+				marker.setIcon(gAssetUrl + '/img/map/icons/finish.png');
+			} else {
+				marker.setIcon(gAssetUrl + '/img/map/icons/spot.png');
+			}
+			path.push(marker.getPosition());
+		}
+	});
+	var ne = new google.maps.LatLng(lat_max, lng_max);
+	var sw = new google.maps.LatLng(lat_min, lng_min);
+	var bounds = new google.maps.LatLngBounds(sw, ne);
+	map.fitBounds(bounds);
+	console.log(path);
+	if (path.length > 0) {
+		linePath = new google.maps.Polyline({
+			map				: map,
+			path			: path,
+			clickable		: false,
+			geodesic		: true,
+			strokeColor		: "#000099",
+			strokeOpacity	: 0.6,
+			strokeWeight	: 6,
+			visible			: true
+		});
+	}
 });
