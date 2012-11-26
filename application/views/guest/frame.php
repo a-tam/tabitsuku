abdcb358f28b61b4fcd76e31b58ec83b60aa1323
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-11-26 11:38:38 +0900

    テンプレートファイルの場所を変更

--- application/views/guest/frame.php
+++ application/views/guest/frame.php
@@ -1,4 +1,3 @@
 <?php $this->load->view("header"); ?>
 <?php $this->load->view($main_page); ?>
-<div style="position:fixed; right: 20px; top:40px;">Template file: <span style="color:#ff0000;"><?php echo FCPATH.APPPATH."views/".$main_page.".php";?></span></div>
 <?php $this->load->view("footer"); ?>
