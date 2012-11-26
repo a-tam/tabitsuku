2bf9e0c53ae2e00c4551c0de93a9557ab73ad2c8
Hiroyuki kiyomizu <hiroyuki.kiyomizu@gmail.com>
2012-11-26 11:37:32 +0900

    テンプレートの場所表示

--- application/views/admin/frame.php
+++ application/views/admin/frame.php
@@ -1,4 +1,4 @@
 <?php $this->load->view("admin/header"); ?>
 <?php $this->load->view($main_page); ?>
-<div style="position:fixed; right: 20px; top:40px;">Template file: <span style="color:#ff0000;"><?php echo FCPATH.APPPATH."views/".$main_page.".php";?></span></div>
+<!-- <div style="position:fixed; right: 20px; top:40px;">Template file: <span style="color:#ff0000;"><?php echo FCPATH.APPPATH."views/".$main_page.".php";?></span></div> -->
 <?php $this->load->view("admin/footer"); ?>
